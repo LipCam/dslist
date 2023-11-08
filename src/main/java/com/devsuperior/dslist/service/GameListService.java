04845fe4bd300c677d9a09888098e6d70b20f702
LipCam <lipcam@hotmail.com.br>
2023-11-08 12:21:02 -0300

    Reordenação da lista de games

--- src/main/java/com/devsuperior/dslist/service/GameListService.java
+++ src/main/java/com/devsuperior/dslist/service/GameListService.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslist.service;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameListRepository;
+import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +16,27 @@ public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
+
+	@Autowired
+	private GameRepository gameRepository;
 		
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		return gameListRepository.findAll().stream().map(x -> new GameListDTO(x)).toList();
+	}
+
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
